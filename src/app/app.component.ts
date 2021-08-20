904b7bc2b1e069b4b5951d7e260cb71bae7ee720
johannpereze <sebas.jsp@gmail.com>
2021-08-19 20:26:54 -0500

    Voy a avanzar en el curso antes de continuar aquí

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngWhatsAppForDentalink';
+  title = 'WhatsApp para Dentalink';
+  names = ['juan', 'luis', 'jose'];
+  addItem() {
+    this.names.push('New Item');
+  }
+  removeItem(index: number) {
+    this.names.splice(index, 1);
+  }
 }
