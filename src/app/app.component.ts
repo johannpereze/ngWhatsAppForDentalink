1508131f13758c31e6c229f80aed686c57117b51
johannpereze <sebas.jsp@gmail.com>
2021-08-31 21:55:57 -0500

    Ahora estamos guardando las claves dentro de la clase del componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 
+interface SecretKeys {
+  dentalinkKey: string;
+  b2chatUser: string;
+  b2ChatPass: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'WhatsApp para Dentalink';
-  names = ['juan', 'luis', 'jose'];
-  addItem() {
-    this.names.push('New Item');
-  }
-  removeItem(index: number) {
-    this.names.splice(index, 1);
+  secretKeys: SecretKeys = {
+    dentalinkKey: '',
+    b2chatUser: '',
+    b2ChatPass: '',
+  };
+
+  saveKeys(event:any) {
+    console.log(this.secretKeys);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
     FormsModule
   ],
   providers: [],
