95fc594ef2ab911f082ab81eac80432a3da3ec63
johannpereze <sebas.jsp@gmail.com>
2021-09-11 14:53:46 -0500

    El componente wa notifications queda refactorizado

--- src/app/wa-notifications/wa-notifications-page/wa-notifications-page.component.ts
+++ src/app/wa-notifications/wa-notifications-page/wa-notifications-page.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-wa-notifications-page',
   templateUrl: './wa-notifications-page.component.html',
   styleUrls: ['./wa-notifications-page.component.scss']
 })
-export class WaNotificationsPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class WaNotificationsPageComponent{
 
 }
