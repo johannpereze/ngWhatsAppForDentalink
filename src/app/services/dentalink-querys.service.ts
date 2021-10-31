581491c34fee33082338a65bdec6cc6ee6abb4b6
johannpereze <sebas.jsp@gmail.com>
2021-10-31 15:02:01 -0500

    Empiezo a mejorar el clinicsList C y hasta ahora el programa funciona excepto por el 57 de la selectedLine

--- src/app/services/dentalink-querys.service.ts
+++ src/app/services/dentalink-querys.service.ts
@@ -34,22 +34,22 @@ export class DentalinkQuerysService {
     appointmentsDate: '', //2022-10-01 puse esta fecha para hacer pruebas y que no descargue siempre 600 citas. Idealmente debería seleccionar 2 días en el futuro, teniendo en cuenta domingos y festivos
   };
 
-  componentVisibility: ComponentVisibility = {
-    secretKeys: false,
-    lineSelection: false,
-    clinicsList: true,
-    templateSelection: false,
-    summary: false,
-    progressBar: false,
-  };
   // componentVisibility: ComponentVisibility = {
-  //   secretKeys: true,
+  //   secretKeys: false,
   //   lineSelection: false,
-  //   clinicsList: false,
+  //   clinicsList: true,
   //   templateSelection: false,
   //   summary: false,
   //   progressBar: false,
   // };
+  componentVisibility: ComponentVisibility = {
+    secretKeys: true,
+    lineSelection: false,
+    clinicsList: false,
+    templateSelection: false,
+    summary: false,
+    progressBar: false,
+  };
 
   loadButtonDisabled = true;
   loadButtonText = 'Espera mientras se descargan las citas';
